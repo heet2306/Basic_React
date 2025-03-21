1033d957c85dd00c7e8ea66ca9a2674ab5b1a0d3
heet2306 <heetmodh2306@gmail.com>
2025-03-21 15:35:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Nav from './Componet/Nav';
+import Main from './Componet/Main';
+import Foot from './Componet/Foot';
 
 function App() {
   return (
     <>
-    Heet Modh
+    <Nav/>
+    <Main/>
+    <Foot/>
     </>
   );
 }
